1bc4d51e7c97769e876923246e3080d090c4de2f
Tim Proffitt <tim.proffitt@gmail.com>
2016-02-15 13:59:32 -0600

    add ui router

--- src/foo.js
+++ src/foo.js
@@ -1,13 +1,20 @@
 var _ = require('script!lodash');
 var angular = require('angular');
+var uirouter = require('ui-router');
 
 console.log('foo')
 console.log('lodash _');
 console.log(_);
 console.log(angular)
+console.log(uirouter)
 document.write("foo application")
 
-angular.module('basicapp', []).config(function() {
+var dependencies = [
+	uirouter
+];
+
+angular.module('basicapp', [uirouter]).config(function($stateProvider) {
  console.log("basicapp with webpack and codesplitting")
+ console.log($stateProvider);
 })
 
