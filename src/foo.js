f170588ade68ace1c7517f6a1b326aab14b3fb25
Tim Proffitt <tim.proffitt@gmail.com>
2016-02-16 19:15:40 -0600

    use ngtemplateloader for  automatically

--- src/foo.js
+++ src/foo.js
@@ -1,6 +1,8 @@
 var _ = require('script!lodash');
 var angular = require('angular');
 var uirouter = require('ui-router');
+var templateUrl = require('ngtemplate!html!./test.html');
+
 
 console.log('foo')
 console.log('lodash _');
@@ -17,4 +19,11 @@ angular.module('basicapp', [uirouter]).config(function($stateProvider) {
  console.log("basicapp with webpack and codesplitting")
  console.log($stateProvider);
 })
+.directive('temptest', Temptest);
+
+function Temptest() {
+	return {
+		templateUrl: templateUrl
+	}
+}
 
